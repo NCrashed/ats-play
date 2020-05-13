d0154750f8bf33979d111d073b696b138e816ba4
Anton Gushcha <ncrashed@gmail.com>
2020-05-13 22:59:54 +0300

    Initial commit

---
+++ pkgs.nix
@@ -0,0 +1,6 @@
+import ((import <nixpkgs> {}).fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs-channels";
+  rev = "683c68232e91f76386db979c461d8fbe2a018782";
+  sha256  = "1fn7q540bk7nhvf6pnlljgfk51qwh2xpvvz30a1d4sd0l7z64mvc";
+})
---
+++ shell.nix
@@ -0,0 +1,19 @@
+# Latest Nightly
+let pkgs = import ./pkgs.nix {};
+in pkgs.stdenv.mkDerivation rec {
+  name = "ats-env";
+  buildInputs = with pkgs; [
+    ats
+
+    # Other packages
+    pkgconfig
+    git
+    libGL
+    xorg.libxcb
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+  ];
+  LD_LIBRARY_PATH = with pkgs; "/run/opengl-driver/lib:/run/opengl-driver-32/lib:${libGL}/lib:${xorg.libX11}/lib:${xorg.libXcursor}/lib:${xorg.libXrandr}/lib:${xorg.libXi}/lib";
+}
